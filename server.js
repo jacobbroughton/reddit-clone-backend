9fdad8ba9d090f5b094111a856309cb1f07d52ee
Jacob Broughton <jlbroughton88@gmail.com>
2023-11-10 05:45:06 -0500

    added domain to origins list to test, only added jacobbroughton.com because i already own it and it wasnt being used. If it does work, i'll change it

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ let sessionStore = new MySQLStore({
 
 app.use(
   cors({
-    origin: ["https://zeddit.onrender.com", "https://zeddit.onrender.com/", "http://localhost:3000"],
+    origin: ["https://zeddit.onrender.com", "https://zeddit.onrender.com/", "http://localhost:3000", "https://www.jacobbroughton.com"],
     credentials: true,
   })
 )
