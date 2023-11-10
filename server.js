e8f2f272055ae71dcd92dab25c36ac041d3d9882
Jacob Broughton <jlbroughton88@gmail.com>
2023-11-10 16:25:18 -0500

    changed api domain

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ let sessionStore = new MySQLStore({
 
 app.use(
   cors({
-    origin: ["https://zeddit.onrender.com", "https://zeddit.onrender.com/", "http://localhost:3000", "https://jacobbroughton.com", "https://www.jacobbroughton.com"],
+    origin: ["api.jacobbroughton.com", "api.jacobbroughton.com/", "http://localhost:3000", "https://jacobbroughton.com", "https://www.jacobbroughton.com"],
     credentials: true,
   })
 )
